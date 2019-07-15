ad07ba53e3d0e8516382808501e48fcc3db5e4a5
weiweimao <506586963@qq.com>
2019-07-15 21:28:57 +0800

    hifsof

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/product/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.stylefeng.guns.rest.modular.product.controller;
+
+
+import com.stylefeng.guns.rest.common.persistence.model.Product;
+import com.stylefeng.guns.rest.modular.product.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author cskaoyan
+ * @since 2019-07-15
+ */
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    IProductService productService;
+
+    @ResponseBody
+    @RequestMapping("/getProductById")
+    public Product getProductById(Integer id){
+
+        Product product = productService.selectById(id);
+
+        return product;
+    }
+
+}
+
